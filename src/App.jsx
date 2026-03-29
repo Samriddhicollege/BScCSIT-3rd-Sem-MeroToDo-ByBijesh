ed0c5fbe430ea515d26a75959add22f43560a28d
bijesh-09 <bijeshkaranjit@gmail.com>
2026-03-29 21:27:42 +0545

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/ThemeContext'
+import { TaskProvider } from './context/TaskProvider'
+import { BrowserRouter as Route, Router, Routes } from 'react-router-dom'
+import { AllTasks } from './pages/AllTasks'
+import { TodayView } from './pages/TodayView'
+import { CategoryView } from './pages/CategoryView'
+import { TrashView } from './pages/TrashView'
+import { Settings } from './pages/Settings'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   return (
-    <div>
-      
-    </div>
+    <ThemeProvider>
+      <TaskProvider>
+        <Router>
+          <Routes>
+            {/* layout route: wraps all pages with navbar and footer */}
+            <Route element={<AllTasks />} >
+              <Route index element={<AllTasks />} />
+            </Route>
+
+            <Route path='/today' element={<TodayView />} />
+            <Route path='/category/:id' element={<CategoryView />} />
+            <Route path='/trash' element={<TrashView />} />
+            <Route path='/settings' element={<Settings />} />
+
+            {/* 404 fallback */}
+            <Route 
+              path='*'
+              element={
+                <div style={{textAlign: 'center', padding: '2rem'}}>
+                  <h1>404 - Page Not Found</h1>
+                  <a href="/">Go back to all tasks</a>
+                </div>
+              }
+            />
+
+          </Routes>
+        </Router>
+      </TaskProvider>
+    </ThemeProvider>
   )
 }
 
